96239e2cc252b89111e8576b61643c10812de6f5
Avinash Bevara <avinash.bevara@techigai.io>
2023-09-15 18:05:18 +0530

    Added UpdateAnswerProc

---
+++ DatabaseProjectCertiTrack/dbo/StoredProcedures/UpdateSolution.sql
@@ -0,0 +1,25 @@
+
+-- =============================================
+-- Author: Avinash Bevara
+-- Create date: 09/15/2023
+-- Description: Update Solution with type 
+-- LastModified: 
+-- =============================================
+CREATE PROCEDURE UpdateSolution (@Type VARCHAR(30), @AnswerId INTEGER, @QuestionID INT, @Answer VARCHAR(max))
+AS
+BEGIN
+	IF (upper(@Type) = 'VOTE')
+	BEGIN
+		UPDATE Solutions
+		SET Votes = votes + 1, LastModifiedOn = getdate()
+		WHERE AnswerID = @AnswerId
+	END
+	ELSE IF (upper(@Type) = 'ANSWER')
+	BEGIN
+		UPDATE Solutions
+		SET Answer = @Answer, QuestionID = @QuestionID, LastModifiedOn = getdate()
+		WHERE AnswerID = @AnswerId
+	END
+END
+GO
+
