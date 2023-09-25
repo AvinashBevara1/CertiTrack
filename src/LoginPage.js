1c0b84332084839b06394c965d013c86a30d2bd6
Ram.Giduturi <ram.giduturi@techigai.io>
2023-09-25 16:02:33 +0530

    frontend src file

---
+++ src/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyComponent from './logincreds';
+
+function Login() {
+        return (
+            <>
+            <div className="login-page">
+                {/* <h1>Login</h1> */}
+                <MyComponent/>
+            </div>
+            </>
+        )
+    }
+
+
+export default Login;
